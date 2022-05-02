9e017edcff8a0bdd01ac673c23e304b7b9d89a60
nicolas novi <nicolasnovi@nicolass-MacBook-Air.local>
2022-05-01 23:12:37 -0300

    agregue la carpeta template,agregue templete a dirt y agregue la app en settings

--- Cine_Blog/AppCine_Blog/views.py
+++ Cine_Blog/AppCine_Blog/views.py
@@ -1,3 +1,10 @@
+from django.template import loader
 from django.shortcuts import render
+from django.http import HttpResponse
+
+#def saluda(request):
+#    plantilla=loader.get_template('primer.html')
+#    return HttpResponse(plantilla.loader({"name"}))
+    
 
 # Create your views here.

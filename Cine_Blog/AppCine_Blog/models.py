2fc39181d7af7526f13bf4948255427cd271d800
nicolas novi <nicolasnovi@nicolass-MacBook-Air.local>
2022-05-02 01:01:42 -0300

    cree los 3 modelos

--- Cine_Blog/AppCine_Blog/models.py
+++ Cine_Blog/AppCine_Blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Usuario(models.Model):
+    nombre=models.CharField('nombre' , max_length=50)
+    apellido=models.CharField('apellido' , max_length=20)
+    email=models.EmailField()
+    contra=models.CharField('contraseña' , max_length=10)
+
+
+class Categoria(models.Model):
+    accion= models.CharField('accion', max_length=50)
+    comedia= models.CharField('comedia',max_length=50)
+    terror= models.CharField('accion',max_length=50)
+    suspenso= models.CharField('accion',max_length=50)
+    aventura= models.CharField('accion',max_length=50)
+    thriller= models.CharField('thriller',max_length=50)
+
+class Pelicula(models.Model):
+    titulo=models.CharField('titulo' , max_length=50)
+    sinopsis=models.CharField('sinopsis' , max_length=20)
+    imagen=models.ImageField()
+    fecha_estreno=models.DateField('fecha de estreno' )
+    duracion= models.IntegerField('duracion')
+    trailer_link= models.CharField('trailer', max_length=256)
+    categoria=models.ForeignKey(Categoria, on_delete=models.CASCADE)
